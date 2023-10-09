e86ca602c5acac19484bc005b685efb62539fc55
Iri Rawal <48539471+Aurdon1234@users.noreply.github.com>
2023-10-09 18:39:33 +0530

    Create Math Functions.c

---
+++ Assignment 2/Math Functions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	float a,b,c,d,e,n;
+	a=cos(0);
+	b=sin(0);
+	c=log(1);
+	n=2;
+	d=pow(4,1/n);
+	e=sqrt(25);
+	printf("%f\n",a);
+	printf("%f\n",b);
+	printf("%f\n",c);
+	printf("%f\n",d);
+	printf("%f\n",e);
+	return 0;
+}
