48aac0a78ca4d42e72c39207b7e6f62332db3bc5
Iri Rawal <48539471+Aurdon1234@users.noreply.github.com>
2023-10-09 18:47:47 +0530

    Create Sizeof Data Type.c

---
+++ Assignment 2/Sizeof Data Type.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	char c;
+	int i;
+	float f;
+	double lf;
+	long int ld;
+	printf("Size of i is %d\n",sizeof(i));
+	printf("Size of c is %d\n",sizeof(c));
+	printf("Size of f is %d\n",sizeof(f));
+	printf("Size of lf is %d\n",sizeof(lf));
+	printf("Size of ld is %d\n",sizeof(ld));
+	return 0;
+}
