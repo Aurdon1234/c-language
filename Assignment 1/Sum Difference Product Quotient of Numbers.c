07ff8c934aba1f5e6bc57af328ab9d25848a2b69
Iri Rawal <48539471+Aurdon1234@users.noreply.github.com>
2023-10-09 18:23:07 +0530

    Create Sum Difference Product Quotient of Numbers.c

---
+++ Assignment 1/Sum Difference Product Quotient of Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("Enter the first number: ");
+	scanf("%d",&a);
+	printf("Enter the second numebr: ");
+	scanf("%d",&b);
+	printf("Sum of the numbers is: %d\n",a+b);
+	printf("Difference of the numbers is: %d\n",a-b);
+	printf("Product of the numbers is: %d\n",a*b);
+	printf("Quotient of the numbers is: %d\n",a/b);
+	return 0;
+}
