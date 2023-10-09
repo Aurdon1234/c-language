273155cb722f088ae4ea929324a919824966f411
Iri Rawal <48539471+Aurdon1234@users.noreply.github.com>
2023-10-09 22:30:05 +0530

    Create Fibonacci Using Functions.c

---
+++ Assignment 4/Fibonacci Using Functions.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void fibonacci(int);
+int main(){
+	int n;
+	scanf("%d",&n);
+	fibonacci(n);
+	return 0;
+}
+void fibonacci(int a){
+	int a1=0,b=1,c,m=2;
+	printf("%d\n",a1);
+	printf("%d\n",b);
+	while(m<a){
+		c=a1+b;
+		printf("%d\n",c);
+		a1=b;
+		b=c;
+		m++;
+	}
+}
