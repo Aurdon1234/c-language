6c19f4df7381c60bfaa9afc288f5546bf7095eb8
Iri Rawal <48539471+Aurdon1234@users.noreply.github.com>
2023-10-09 18:20:58 +0530

    Create Input Fraction.c

---
+++ Assignment 1/Input Fraction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("Enter the numerator: ");
+	scanf("%d",&a);
+	printf("Enter the denominator: ");
+	scanf("%d",&b);
+	printf("The inputed fraction is: %d/%d",a,b);
+	return 0;
+}
