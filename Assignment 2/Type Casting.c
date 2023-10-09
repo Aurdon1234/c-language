f28710f5edbacf165a2688016a48f5268421e56c
Iri Rawal <48539471+Aurdon1234@users.noreply.github.com>
2023-10-09 18:48:22 +0530

    Create Type Casting.c

---
+++ Assignment 2/Type Casting.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	float sum,n,mean;
+	scanf("%f",&sum);
+	scanf("%f",&n);
+	mean=sum/n;
+	printf("%f\n",mean);
+	printf("Value of mean= %d",(int) mean);
+	return 0;
+}
